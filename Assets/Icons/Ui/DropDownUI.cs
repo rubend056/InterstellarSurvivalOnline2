1d1d5468b15a558b11bb89b0e3ca97ecf8ce073f
Ruben Prieto <rubendariopm14@gmail.com>
2017-09-04 22:32:53 -0400

    still adding files

---
+++ Assets/Icons/Ui/DropDownUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownUI : MonoBehaviour {
+
+	public int maxItems = 50;
+	List<GameObject> items;
+	// Use this for initialization
+	void Start () {
+		items = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool addItem (GameObject toAdd){
+		return true;
+	}
+	public void toggle(){
+		
+	}
+		
+}
